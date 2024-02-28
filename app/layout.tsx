9f16e7005ceb88128b468f42761d84b3d698892e
Luigi Lupini <luigi.lupini@gmail.com>
2024-02-28 08:36:30 +0200

    Completed project template

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import { auth } from "@/auth"
 import { ThemeToggle } from "@/components/theme-toggle"
-import { inter } from "@/lib/typeface/fonts"
+import { geist } from "@/lib/typeface/fonts"
 import { cn } from "@/lib/utils"
 import ProviderTree from "@/state/context/provider-tree"
-import type { Metadata } from "next"
 import { PropsWithChildren } from "react"
 
 import "@/styles/globals.css"
@@ -13,7 +12,7 @@ export default async function RootLayout({ children }: PropsWithChildren) {
   return (
     <html
       lang="en"
-      className={cn("antialiased", inter.className)}
+      className={cn("antialiased", geist.className)}
       suppressHydrationWarning
     >
       <body className="screen-full flex overflow-hidden">
