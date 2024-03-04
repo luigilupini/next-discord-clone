a74e67bda86d79d85c2b809f26194a46e5d4b2b8
Luigi Lupini <luigi.lupini@gmail.com>
2024-03-04 08:47:59 +0200

    Removed pusher configuration

--- app/socket/page.tsx
+++ app/socket/page.tsx
@@ -1,5 +1,4 @@
 import Footer from "@/components/footer"
-import JoinForm from "@/components/form/join-form"
 import GridBase, {
   GridBody,
   GridFooter,
@@ -10,17 +9,14 @@ import SocketIndicator from "@/components/socket/socket-indicator"
 export default function HomePage() {
   return (
     <GridBase layout="basic">
-      <GridBody className="-mt-2">
-        <GridHeader className="py-2 pr-2">
-          <h1 className="text-2xl font-bold">Socket.io</h1>
-          <div className="flex items-center justify-center gap-2">
-            <SocketIndicator />
-          </div>
-        </GridHeader>
-        <article className="relative flex size-full flex-col items-end justify-center gap-4 px-4">
-          <JoinForm />
-        </article>
-      </GridBody>
+      <GridHeader className="py-2 pr-2">
+        <h1 className="text-2xl font-bold">Socket.io</h1>
+        <div className="flex items-center justify-center gap-2">
+          <SocketIndicator />
+        </div>
+      </GridHeader>
+
+      <GridBody className="-mt-2">Body here!</GridBody>
       <GridFooter>
         <Footer />
       </GridFooter>
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,18 +1,10 @@
 "use client"
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Button } from "@/components/ui/button"
 import { Card } from "@/components/ui/card"
-import { initials } from "@/lib/utils"
 import { UserButton } from "@clerk/nextjs"
 import { motion } from "framer-motion"
-import { DoorClosed, DoorOpen } from "lucide-react"
-import { Session } from "next-auth"
-import Link from "next/link"
-import { useState } from "react"
 
 export default function Footer() {
-  // const { data: session } = useSession()
   return (
     <motion.footer
       className="w-full"
@@ -26,55 +18,7 @@ export default function Footer() {
     >
       <Card className="flex w-full px-1 py-[3px] shadow-none">
         <UserButton afterSignOutUrl="/" />
-        {/* {session && <Authorized session={session} />} */}
       </Card>
     </motion.footer>
-  )
-}
-
-function Authorized({ session }: { session: Session }) {
-  const [hover, setHover] = useState(false)
-  const { user } = session
-  return (
-    <article className="flex w-full items-center justify-between">
-      <div className="flex items-center gap-2">
-        <Avatar className="size-10 rounded-full shadow-sm">
-          <div className="size-full rounded-full border-[1px] border-primary/80">
-            <AvatarImage
-              src={session!.user!.image!}
-              alt="@user"
-              className="rounded-full border-[2px] border-transparent"
-            />
-            <AvatarFallback className="rounded-full border-[2px] border-primary">
-              {user?.name && initials(user?.name)}
-            </AvatarFallback>
-          </div>
-        </Avatar>
-        <p className="flex flex-col">
-          <span className="inline-block max-w-[195px] truncate text-start text-[13px] font-semibold">
-            {user?.name}
-          </span>
-          <span className="-mt-[1px] inline-block max-w-[195px] truncate text-start text-[12px]">
-            {user?.email}
-          </span>
-        </p>
-      </div>
-
-      <Button
-        asChild
-        variant="ghost"
-        onMouseLeave={() => setHover(false)}
-        onMouseEnter={() => setHover(true)}
-      >
-        <Link
-          href="/api/auth/signout"
-          className="flex items-center justify-center gap-2 text-sm font-normal opacity-70 transition-all duration-300 ease-in-out hover:bg-secondary hover:fill-primary"
-        >
-          Leave
-          {hover && <DoorOpen size={18} className="animate-pulse" />}
-          {!hover && <DoorClosed size={18} />}
-        </Link>
-      </Button>
-    </article>
   )
 }
